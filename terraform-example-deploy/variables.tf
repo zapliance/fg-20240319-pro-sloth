d580c376e80c1ed6906fc0755850942daffd9bf3
felix <felix.greve@zapliance.com>
2024-03-19 15:46:06 +0100

    Testcommit mit Beispielcontainer App Faultier ! :)

--- terraform-example-deploy/variables.tf
+++ terraform-example-deploy/variables.tf
@@ -1,3 +1,31 @@
 variable "resource_group_name" {
   type = string
 }
+
+variable "container_group_name" {
+  default = "zapConteinerGroup"
+}
+
+
+variable "container_name" {
+  default = "zapPostgres"
+}
+
+variable "container_image" {
+  default = "mcr.microsoft.com/azuredocs/aci-helloworld"
+}
+variable "container_cpu" {
+  default = "2"
+}
+variable "container_memory" {
+  default = "2"
+}
+variable "container_port" {
+  default = "80"
+}
+
+variable "tags" {
+  default = {
+    zapTag = "zapValue"
+  }
+}
--- terraform-example-deploy/virtual_machine.tf
+++ terraform-example-deploy/virtual_machine.tf
@@ -1,62 +1,25 @@
-data "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "zapExample" {
   name = var.resource_group_name
 }
+resource "azurerm_container_group" "zapName" {
+    name                = var.container_group_name
+    location            = data.azurerm_resource_group.zapExample.location
+    resource_group_name = data.azurerm_resource_group.zapExample.name
+    os_type             = "Linux"
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-}
+    ip_address_type = "Public"
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+    container {
+        name   = var.container_name
+        image  = var.container_image
+        cpu    = var.container_cpu
+        memory = var.container_memory
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+        ports {
+        port     = var.container_port
+        protocol = "TCP"
+        }
+    }
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "tls_private_key" "main" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = tls_private_key.main.public_key_openssh
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+    tags = var.tags
 }
